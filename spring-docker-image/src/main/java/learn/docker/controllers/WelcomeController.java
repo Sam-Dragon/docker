10606b9a2c8125f60047350576de84a98227ec1d
Admin <Admin@host.docker.internal>
2021-05-14 20:03:42 +0530

    Added files

---
+++ spring-docker-image/src/main/java/learn/docker/controllers/WelcomeController.java
@@ -0,0 +1,15 @@
+package learn.docker.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class WelcomeController {
+
+	@GetMapping
+	public String hello() {
+		return "Welcome to Docker world";
+	}
+}
