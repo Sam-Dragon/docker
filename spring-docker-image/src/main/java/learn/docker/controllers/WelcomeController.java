1766a088f4dc40ea2c646de060c39610bcf75931
Admin <Admin@host.docker.internal>
2021-05-15 17:55:34 +0530

    Docker configuration added

--- spring-docker-image/src/main/java/learn/docker/controllers/WelcomeController.java
+++ spring-docker-image/src/main/java/learn/docker/controllers/WelcomeController.java
@@ -1,5 +1,7 @@
 package learn.docker.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class WelcomeController {
+	private static Logger logger = LoggerFactory.getLogger(WelcomeController.class);
 
 	@GetMapping
 	public String hello() {
+		logger.info("Welcome to Docker world");
+
 		return "Welcome to Docker world";
 	}
 }
